5cb138332b4e191cd92c7c55e06bcfa03daa7f75
Bode512 <bode84886@gmail.com>
2026-06-09 21:20:17 +0200

    add_task

--- BuscaminasCLI/main.py
+++ BuscaminasCLI/main.py
@@ -1,17 +1,15 @@
-print ("hola hijos de puta que tal como os va la vida")
+print("Hola, bienvenido al Buscaminas CLI")
 
 
 def add_task():
-    #este es mini todo-list
-    # ADD TASK
+    # este es mini todo-list
     task = input("Enter a task: ")
     with open("tasks.txt", "a") as file:
         file.write(task + "\n")
     print("Task added successfully!")
-    
 
-def main ():
-    #....
+
+def main():
     while True:
         print("1. Add Task")
         print("2. Exit")
@@ -19,12 +17,12 @@ def main ():
         if choice == "1":
             add_task()
         elif choice == "2":
-            print("Exiting...")
+            print("Exiting the program. Goodbye!")
             break
         else:
             print("Invalid choice. Please try again.")
 
-#dale duro vamos a ello
+# dale duro vamos a ello
 
 
 if __name__ == '__main__':
